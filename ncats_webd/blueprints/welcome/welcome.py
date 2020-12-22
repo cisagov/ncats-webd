e018f20d3bba7786c02a3f0fb9c0db80ced50b7e
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2020-12-22 14:52:17 -0500

    blackened ncats_webd/blueprints/welcome/*.py

--- ncats_webd/blueprints/welcome/welcome.py
+++ ncats_webd/blueprints/welcome/welcome.py
@@ -3,37 +3,44 @@ from flask_socketio import join_room, leave_room
 
 from ncats_webd.common import cache, socketio
 
-BLUEPRINT_NAME = 'welcome'
-bp = Blueprint(BLUEPRINT_NAME, __name__,
-               template_folder='templates',
-               static_folder='static')
+BLUEPRINT_NAME = "welcome"
+bp = Blueprint(
+    BLUEPRINT_NAME, __name__, template_folder="templates", static_folder="static"
+)
 
-@bp.route('/')
+
+@bp.route("/")
 def root():
-    return render_template('welcome.html')
+    return render_template("welcome.html")
 
-@bp.route('/rr')
+
+@bp.route("/rr")
 def coming_soons():
-    return render_template('soon.html')
+    return render_template("soon.html")
 
-@bp.route('/prefs')
+
+@bp.route("/prefs")
 def preferences():
-    return render_template('prefs.html')
+    return render_template("prefs.html")
 
-@socketio.on('connect', namespace='/cyhy')
+
+@socketio.on("connect", namespace="/cyhy")
 def sys_connect():
-    current_app.logger.info('client connected')
+    current_app.logger.info("client connected")
 
-@socketio.on('disconnect', namespace='/cyhy')
+
+@socketio.on("disconnect", namespace="/cyhy")
 def sys_disconnect():
-    current_app.logger.info('client disconnected')
+    current_app.logger.info("client disconnected")
 
-@socketio.on('join', namespace='/cyhy')
-def join(message):
-    join_room(message['room'])
-    current_app.logger.info('client joined room %s' % message['room'])
 
-@socketio.on('unjoin', namespace='/cyhy')
+@socketio.on("join", namespace="/cyhy")
 def join(message):
-    leave_room(message['room'])
-    current_app.logger.info('client left room %s' % message['room'])
+    join_room(message["room"])
+    current_app.logger.info("client joined room %s" % message["room"])
+
+
+@socketio.on("unjoin", namespace="/cyhy")
+def join(message):
+    leave_room(message["room"])
+    current_app.logger.info("client left room %s" % message["room"])
